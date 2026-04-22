5d127074b4b055bc4a8044cedd6e065e2679e217
xuxiang <2686987168@qq.com>
2026-04-22 23:15:03 +0800

    增加输出内容

--- chapter08-2/src/test/java/Egg01Test.java
+++ chapter08-2/src/test/java/Egg01Test.java
@@ -11,6 +11,34 @@ import java.io.InputStream;
 import java.io.Reader;
 
 public class Egg01Test {
+
+    @Test
+    public void testDBPool(){
+        try {
+            InputStream inputStream = Resources.getResourceAsStream("mapper/mybatis-config.xml");
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+//            一个数据库创建一个SqlSessionFactory对象，不需要每次创建SqlSession对象都重新创建
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(inputStream, "development");
+//            会话1
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            User result = sqlSession.selectOne("com.itheima.Example.mapper.UserMapper.findById", 1);
+            System.out.println(result);
+            sqlSession.close();
+
+//            会话2
+            SqlSession sqlSession2 = sqlSessionFactory.openSession();
+            User result2 = sqlSession2.selectOne("com.itheima.Example.mapper.UserMapper.findById", 1);
+            System.out.println(result2);
+            //            关闭会话对象
+            sqlSession2.close();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+           }
+
     @Test
     public void testEgg01(){
         try {
@@ -25,10 +53,9 @@ public class Egg01Test {
             //            调用sqlSession对象的selectOne()方法,执行查询操作句
             User result = sqlSession.selectOne("com.itheima.Example.mapper.UserMapper.findById", 1);
             System.out.println(result);
-            System.out.println("hhhhh");
+            System.out.println("hhhhh111111");
             //            关闭会话对象
             sqlSession.close();
-
         } catch (Exception e) {
             e.printStackTrace();
         }
